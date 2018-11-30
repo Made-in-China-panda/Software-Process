e7ce38d264a34a499af58cc33dadb925befcbae3
liuyingjie1130 <1410505543@qq.com>
2018-11-30 09:31:27 +0800

    吃

---
+++ src/pages/about/about.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { SearchPage} from '../search/search';
+@Component({
+  selector: 'page-about',
+  templateUrl: 'about.html'
+})
+
+export class AboutPage {
+  item=[12,2,3];
+  
+ 
+  constructor(public navCtrl: NavController) {
+   
+  }
+search(){
+  this.navCtrl.push(SearchPage);
+}
+
+
+
+
+
+
+}
+
+// export class Food{
+//   constructor(
+//     public idFood:'001',
+//     public name:'香辣花蛤',
+//     public pic:'G:\攻略说\imgs\鲁\1香辣花蛤\1.jpg',
+//     public  collection:'0',
+//     public  material:'花蛤'
+//   ){}
+// }
