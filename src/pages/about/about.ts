ae01e5bc8d4caf14bbe729fb952ae5d0975f1bfd
liuyingjie1130 <1410505543@qq.com>
2018-11-30 09:11:39 +0800

    ch

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -5,12 +5,36 @@ import { SearchPage} from '../search/search';
   selector: 'page-about',
   templateUrl: 'about.html'
 })
+
 export class AboutPage {
-
+  item=[12,2,3];
+ food={
+    idFood:'001',
+    name:'香辣花蛤',
+    pic:'G:\攻略说\imgs\鲁\1香辣花蛤\1.jpg',
+    collection:'0',
+    material:'花蛤'
+  }
   constructor(public navCtrl: NavController) {
-
+   
   }
 search(){
   this.navCtrl.push(SearchPage);
 }
+
+
+
+
+
+
 }
+
+// export class Food{
+//   constructor(
+//     public idFood:'001',
+//     public name:'香辣花蛤',
+//     public pic:'G:\攻略说\imgs\鲁\1香辣花蛤\1.jpg',
+//     public  collection:'0',
+//     public  material:'花蛤'
+//   ){}
+// }
