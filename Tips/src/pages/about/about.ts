0249bef521c42c318a6a9375026b96889749b03a
liuyingjie1130 <1410505543@qq.com>
2018-12-03 18:18:56 +0800

    all

---
+++ Tips/src/pages/about/about.ts
@@ -0,0 +1,59 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { SearchPage} from '../search/search';
+import { SweetPage } from '../sweet/sweet';
+import { LuPage } from '../lu/lu';
+import { ChuanPage } from '../chuan/chuan';
+import { SuPage } from '../su/su';
+import { YuePage } from '../yue/yue';
+import {FoodstepPage } from '../foodstep/foodstep';
+@Component({
+  selector: 'page-about',
+  templateUrl: 'about.html'
+})
+
+export class AboutPage {
+  num;
+  items=[1,2,3];
+  food={
+    idFood:'001',
+    name:'香辣花蛤',
+    taste:'蒜香',
+    collection:'0',
+    pic:'assets/imgs/美食/tt.jpg',
+    material:'花蛤'
+  }
+ 
+  constructor(public navCtrl: NavController) {
+   
+  }
+search(){
+  this.navCtrl.push(SearchPage);
+}
+goSweet(){
+  this.navCtrl.push(SweetPage);
+}
+goRibang(){this.num=0;}
+goYuebang(){this.num=1;}
+goLu(){this.navCtrl.push(LuPage);}
+goChuan(){this.navCtrl.push(ChuanPage);}
+goSu(){this.navCtrl.push(SuPage);}
+goYue(){this.navCtrl.push(YuePage);}
+goFoodstep(){
+  this.navCtrl.push(FoodstepPage);
+}
+
+
+
+
+}
+
+// export class Food{
+//   constructor(
+//     public idFood:'001',
+//     public name:'香辣花蛤',
+//     public pic:'G:\攻略说\imgs\鲁\1香辣花蛤\1.jpg',
+//     public  collection:'0',
+//     public  material:'花蛤'
+//   ){}
+// }
